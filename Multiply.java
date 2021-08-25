550b161fa7ffcfa6ed93a71859f5a59f07bfaabb
Abdullah Aleem <Abdullah.aleem6@gmail.com>
2021-08-26 03:15:25 +0530

    Multiply functionality added

---
+++ Multiply.java
@@ -0,0 +1,12 @@
+package First;
+
+public class Multiply {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5;
+		int b =10;
+		System.out.println("happy multiply: "+(a*b));
+	}
+
+}
